6886fa19bdc794599bb48e0d0487f91402c7e6fa
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-22 16:48:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,10 @@ def key_for_min_value(name_hash)
 Minimum = nil
 
 name_hash.each do |key, value|
-  if min == nil
-    min = key
-  elsif value < name_hash[key]
-    min = key
+  if Minimum == nil
+    Minimum = key
+  elsif value < name_hash[Minimum]
+    Minimum = key
   end
 end
 
